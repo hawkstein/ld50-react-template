2899963090cc996d91f05a6fb3413e0ef46591ca
hawkstein <liamhawks+liamhawks@gmail.com>
2022-03-28 20:20:59 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import s from "./App.module.css";
+
+function App() {
+  return (
+    <div className={s.App}>
+      <header>
+        <h1>Ludum Dare 50 Untitled Entry</h1>
+      </header>
+      <main>
+        <p>Game goes here!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
